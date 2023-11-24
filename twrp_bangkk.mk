f59e61f48d1bedf711da3262c7701d15869b904a
chankruze <chankruze@gmail.com>
2023-11-24 12:34:15 +0530

    add twrp makefile
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ twrp_bangkk.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2019 The TwrpBuilder Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/motorola/bangkk
+
+# Inherit from device.mk configuration
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := bangkk
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := bangkk
+PRODUCT_NAME := twrp_bangkk
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto g84 5G
+PRODUCT_MANUFACTURER := motorola
+
+# Assert (bangkk,XT2347-2)
+TARGET_OTA_ASSERT_DEVICE := bangkk # bangkk,XT2347-2
